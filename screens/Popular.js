7dca3b066e0903388fa5a5114ba7623cf0d0e69b
vishalgaddam873 <vishal18@navgurukul.org>
2020-11-17 14:09:26 +0530

    Initial Comment

---
+++ screens/Popular.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+import { Card } from "react-native-elements";
+import axios from "axios";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default class PopularArticlesScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    const url = "http://localhost:5000/popular-articles";
+    axios
+      .get(url)
+      .then(async response => {
+        this.setState({ data: response.data.data });
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
+  renderItems = ({ item, index }) => {
+    return (
+      <Card
+        key={`card-${index}`}
+        title={item.title}
+        containerStyle={styles.cardContainer}
+      ></Card>
+    );
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={data}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderItems}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+
+  cardContainer: {
+    flex: 1,
+    borderRadius: RFValue(10),
+    justifyContent: "center",
+    height: RFValue(110),
+    marginBottom: RFValue(20)
+  }
+});
